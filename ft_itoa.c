0a49e0a77b0adb5514ab2777dbe6a11d27b947e9
Matthew Gould <matt@everettgould.com>
2016-12-04 12:41:35 -0800

    needed to handle the zero case and malloc the right size to pass itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/30 11:38:25 by mgould            #+#    #+#             */
-/*   Updated: 2016/11/30 13:36:34 by mgould           ###   ########.fr       */
+/*   Updated: 2016/12/04 12:31:51 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,26 @@
 
 char	*ft_itoa(int n)
 {
-	int		i;
-	int		len;
-	int		copy;
-	char	*num;
+	int			i;
+	int			len;
+	long		copy;
+	char		*num;
 
 	i = 0;
 	len = 0;
+	num = NULL;
 	copy = n;
+	if (n == 0)
+	{
+		if ((num = (char *)malloc(sizeof(char) * (2))))
+		{
+			num[0] = '0';
+			num[1] = '\0';
+			return (num);
+		}
+		else
+			return (NULL);
+	}
 	if (copy < 0)
 		len++;
 	while (copy != 0)
@@ -29,8 +41,9 @@ char	*ft_itoa(int n)
 		len++;
 		copy = copy / 10;
 	}
-	num = (char *)malloc(sizeof(char) * (len + 1));
-	num[len + 1] = '\0';
+	if (!(num = (char *)malloc(sizeof(char) * (len + 1))))
+			return (NULL);
+	num[len] = '\0';
 	copy = n;
 	if (copy < 0)
 	{
@@ -40,9 +53,9 @@ char	*ft_itoa(int n)
 	}
 	while (copy != 0)
 	{
-		num[len] = ((copy % 10) + '0');
+        len--;
+        num[len] = ((copy % 10) + '0');
 		copy = copy / 10;
-		len--;
 	}
 	return (num);
 }
